a4e29dd2b7406dbabaa558735b7c2c8adb9b9327
PaulFrankling <franky.p@hotmail.co.uk>
2021-05-02 14:24:55 +0000

    Comments added

--- static/js/topButton.js
+++ static/js/topButton.js
@@ -1,17 +1,20 @@
 //  Credit: https://forum.squarespace.com/topic/190117-back-to-top-button-make-it-appeardisappear-on-scroll/
 
+// Back to top button
 $(document).ready(function () {
-       $(window).scroll(function () {
-           if ($(this).scrollTop() > 200) {
-               $('a.btn-floating').fadeIn();
-           } else {
-               $('a.btn-floating').fadeOut();
-           }
-       });
-       $('.btn-floating').click(function () {
-           $("html, body").animate({
-               scrollTop: 0
-           }, 200);
-           return false;
-       });
-   });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('a.btn-floating').fadeIn();
+        } else {
+            $('a.btn-floating').fadeOut();
+        }
+    });
+    $('.btn-floating').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 200);
+        return false;
+    });
+
+});
