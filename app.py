eee4baba83209c10017a6022ab9e338be5eeede7
PaulFrankling <franky.p@hotmail.co.uk>
2021-05-04 19:31:02 +0000

    Added 500.html and added errorhandlers

--- app.py
+++ app.py
@@ -197,6 +197,18 @@ def logout():
     return redirect(url_for("login"))
 
 
+# 404 Error Page
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html", error=error), 404
+
+
+# 500 Error Page
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("500.html", error=error), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
